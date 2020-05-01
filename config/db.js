326e6f79e1e103e64b73b24804bec7f915a319e8
Dhanu <dhanumca3748@gmail.com>
2020-05-02 02:11:13 +0530

    Cloud DB Created & Azure Hosting Initiated

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 
 
 module.exports= {
-    uri:"mongodb://localhost:27017/MXiansPortal",
+    //uri:"mongodb://localhost:27017/MXiansPortal",
+    uri:"mongodb+srv://dhanumdp:mcadhanu@dhanumdp-brslm.mongodb.net/MXiansPortal?retryWrites=true&w=majority",
     secret:crypto
 }
